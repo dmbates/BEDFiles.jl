01a0b698d19f52d880ffaf4ca28b2ceccd81027e
Douglas Bates <dmbates@gmail.com>
2018-07-05 08:45:39 -0500

    Switch to BEDFile <: AbstractMatrix

--- src/bedfile.jl
+++ src/bedfile.jl
@@ -4,7 +4,7 @@
 Raw .bed file as a shared, memory-mapped Matrix{UInt8}.  The number of rows, `m`
 is stored separately because it is not uniquely determined by the size of the `data` field.
 """
-struct BEDFile
+struct BEDFile <: AbstractMatrix{UInt8}
     data::Matrix{UInt8}
     m::Int
 end
@@ -26,16 +26,33 @@ BEDFile(nm::AbstractString) = BEDFile(nm, countlines(string(splitext(nm)[1], ".f
 
 Return a matrix of frequency counts for the columns
 """
-function columncounts(f::BEDFile)
+columncounts(f::BEDFile) = colcounts!(zeros(Int, (4, size(f, 2))), f)
+
+function colcounts!(counts, f::BEDFile)
     m, n = size(f)
-    counts = Matrix{Int}(undef, (4, n))
-    Threads.@threads for j in 1:n
-        counts!(view(counts, :, j), BEDColumn(f, j))
-    end
+    @inbounds for j in 1:n
+        for i in 1:m
+            counts[f[i, j] + 1, j] += 1
+        end
+    end    
     counts
+end    
+
+function Base.getindex(f::BEDFile, i::Integer, j::Integer)
+    ip3 = i + 3
+    (f.data[ip3 >> 2, j] >> ((ip3 & 0x03) << 1)) & 0x03
 end
 
-Base.getindex(g::BEDFile, i::Integer, j::Integer) = BEDColumn(g, j)[i]
+Base.eltype(f::BEDFile) = UInt8
+
+Base.length(f::BEDFile) = f.m * size(f.data, 2)
+
+Base.IndexStyle(::Type{BEDFile}) = CartesianIndex()
+
+Base.size(f::BEDFile) = f.m, size(f.data, 2)
+
+Base.size(f::BEDFile, k::Integer) = 
+    k == 1 ? f.m : k == 2 ? size(f.data, 2) : k > 2 ? 1 : error("Dimension out of range")
 
 """
     outer(f::BEDFile, colinds)
@@ -47,33 +64,49 @@ The `colinds` argument, when given, causes the operation to be performed on that
 of the columns.
 """
 function outer(f::BEDFile, colinds::AbstractVector{<:Integer})
-    m, n = size(f)
-    outer!(Symmetric(zeros(Float32, (m,m))), f, colinds)
+    m = size(f, 1)
+    outer!(Symmetric(zeros(Float32, (m, m))), f, colinds)
 end
-outer(f::BEDFile) = outer(f, 1:size(f)[2])
+outer(f::BEDFile) = outer(f, 1:size(f, 2))
+
+function Base.copyto!(v::AbstractVector{T}, f::BEDFile, j) where T <: AbstractFloat
+    for i in 1:f.m
+        fij = f[i, j]
+        v[i] = iszero(fij) ? zero(T) : isone(fij) ? T(NaN) : fij - 1
+    end
+    v
+end
 
 """
     outer!(sy::Symmetric, f::BEDFile, colinds)
 
 update `sy` with the sum of the outer products of the columns in `colind` from `f`
 """
-function outer!(sy::Symmetric{T}, f::BEDFile, colinds::AbstractVector{<:Integer}) where T <: AbstractFloat
-    m, n = size(f)
-    tempv = Vector{T}(undef, m)
+function outer!(sy::Symmetric{T}, f::BEDFile, colinds::AbstractVector{<:Integer}) where T
+    tempv = Vector{T}(undef, f.m)
     for j in colinds
-        LinearAlgebra.BLAS.syr!(sy.uplo, 1.0f0, copyto!(tempv, BEDColumn(f, j)), sy.data)
+        LinearAlgebra.BLAS.syr!(sy.uplo, one(T), copyto!(tempv, f, j), sy.data)
     end
     sy
 end
-
-Base.size(f::BEDFile) = f.m, size(f.data, 2)
 
 function Statistics.mean(f::BEDFile; dims)
     m, n = size(f)
     isone(dims) || throw(ArgumentError("mean(f::BEDFile; dims) only defined for dims = 1"))
     means = Matrix{Float64}(undef, (1, n))
-    Threads.@threads for j in 1:n
-        @inbounds means[j] = mean(BEDColumn(f, j))
+    @inbounds for j in 1:n
+        s = 0.0
+        nnmiss = 0
+        for i in 1:m
+            fij = f[i, j]
+            if fij ≠ 1
+                nnmiss += 1
+            end
+            if fij > 1
+                s += fij - 1
+            end
+        end
+        means[j] = s/nnmiss
     end
     means
 end
