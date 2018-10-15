a42f48f6c41edc0eb9fee1fc7a25f68a47cfc797
Hua Zhou <huazhou@ucla.edu>
2018-10-14 19:38:09 -0700

    `filter` BEDFile according to success rates; `filter` Plink files according to row and column indices

--- src/bedfile.jl
+++ src/bedfile.jl
@@ -264,7 +264,8 @@ function Base.copyto!(
     v::AbstractMatrix{T}, 
     f::BEDFile, 
     colinds::AbstractVector{<:Integer};
-    kwargs...) where T <: AbstractFloat
+    kwargs...
+    ) where T <: AbstractFloat
     for (vj, j) in enumerate(colinds)
         Base.copyto!(view(v, :, vj), f, j; kwargs...)
     end
@@ -388,3 +389,111 @@ function grm(
     end
     Φ
 end # function grm
+
+"""
+    BEDFiles.filter(A[, min_success_rate_per_row, min_success_rate_per_col, maxiters])
+
+Filter a BEDFile by genotyping success rate.
+
+# Input
+- `A`: a BEDFile.
+- `min_success_rate_per_row`: threshold for SNP genotyping success rate.
+- `min_success_rate_per_col`: threshold for person genotyping success rate.
+- `maxiters`: maximum number of filtering iterations.
+
+# Output
+- `rmask`: Bool vector indicating remaining rows.
+- `cmask`: Bool vector indicating remaining cols.
+"""
+function filter(
+    f::BEDFile, 
+    min_success_rate_per_row::Real = 0.98,
+    min_success_rate_per_col::Real = 0.98,
+    maxiters::Integer = 5)
+    m, n = size(f)
+    rc, cc = zeros(Int, m), zeros(Int, n)
+    rmask, cmask = trues(m), trues(n)
+    rmiss, cmiss = 1 - min_success_rate_per_row, 1 - min_success_rate_per_col
+    for iter in 1:maxiters
+        fill!(rc, 0)
+        fill!(cc, 0)
+        @inbounds for j in 1:n
+            cmask[j] || continue
+            for i in 1:m
+                rmask[i] || (f[i, j] == 0x01) && (rc[i] += 1; cc[j] += 1)
+            end
+        end
+        rows, cols = count(rmask), count(cmask)
+        @inbounds for j in 1:n
+            cmask[j] = cmask[j] && (cc[j] < (1 - min_success_rate_per_col) * rows)
+        end
+        @inbounds for i in 1:m
+            rmask[i] = rmask[i] && (rc[i] < (1 - min_success_rate_per_row) * cols)
+        end
+        count(cmask) == cols && count(rmask) == rows && break
+        iter == maxiters && (@warn "success rate not satisfied; consider increase maxiters")
+    end
+    rmask, cmask
+end
+
+"""
+    BEDFiles.filter(src, rowinds, colinds; des = src * ".filtered")
+
+Filter `src` Plink files according to row indices `rowinds` and column indices 
+`colinds` and write to a new set of Plink files `des`.
+
+# Input
+- `src`: source Plink file name without suffix ".bed", ".fam" or ".bim".
+- `rowinds`: row indices.
+- `colinds`: column indices.
+
+# Keyword arguments
+- `des`: output Plink file name; defualt is `src * ".filtered"`.
+"""
+function filter(
+    src::AbstractString, 
+    rowinds::AbstractVector{<:Integer},
+    colinds::AbstractVector{<:Integer};
+    des::AbstractString = src * ".filtered")
+    # check source plink files
+    isfile(src * ".bed") || throw(ArgumentError("$src.bed file not found"))
+    isfile(src * ".bim") || throw(ArgumentError("$src.bim file not found"))
+    isfile(src * ".fam") || throw(ArgumentError("$src.fam file not found"))
+    # create row and column masks
+    if eltype(rowinds) == Bool
+        rmask = rowinds
+    else
+        rmask = falses(countlines(src * ".fam"))
+        rmask[rowinds] .= true
+    end
+    if eltype(colinds) == Bool
+        cmask = colinds
+    else
+        cmask = falses(countlines(src * ".bim"))
+        cmask[colinds] .= true
+    end
+    m, n = count(rmask), count(cmask)
+    bfsrc = BEDFile(src * ".bed")
+    # write filtered bed file
+    io = open(des * ".bed", "w+")
+    write(io, 0x1b6c)
+    write(io, 0x01)
+    write(io, fill(0x00, ((m + 3) >> 2, n)))
+    close(io)
+    bfdes = BEDFile(des * ".bed", m, "r+")
+    bfdes .= @view bfsrc[rmask, cmask]
+    # write filtered fam file
+    out = open(des * ".fam", "w")
+    for (i, line) in enumerate(eachline(src * ".fam"))
+        rmask[i] && println(out, line)
+    end
+    close(out)
+    # write filtered bim file
+    out = open(des * ".bim", "w")
+    for (j, line) in enumerate(eachline(src * ".bim"))
+        cmask[j] && println(out, line)
+    end
+    close(out)
+    # output BEDFile
+    bfdes
+end
